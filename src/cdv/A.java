2722c42e0fa9dc8d541e03c112c7a4242f995e75
a18846013664 <1714993997@qq.com>
2020-06-29 11:22:34 +0800

    add class ‘A’

---
+++ src/cdv/A.java
@@ -0,0 +1,17 @@
+package cdv;
+
+public class A {
+    private String x = "123";
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public A(String x) {
+        this.x = x;
+    }
+}
